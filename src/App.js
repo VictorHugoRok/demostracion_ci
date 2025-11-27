9fc6a18769d83436abbfcbe05b612db4cb752e53
Victor <CesarRK77@hotmail.com>
2025-11-27 14:38:57 -0600

    demostracion aqui

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aqui se prueba la nyeva rama y el pull
+          Aqui realizo un cambio nuevo
         </p>
         <a
           className="App-link"
