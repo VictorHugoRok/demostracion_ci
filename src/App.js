d9923be26cba7280081820ebcfa33b1f96749dd6
Victor <CesarRK77@hotmail.com>
2025-11-27 14:00:24 -0600

    prueba

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Aqui hay un nuevo cambio
+          Volvemos a hacer cambios
         </p>
         <a
           className="App-link"
