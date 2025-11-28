d48283a64b9cdd28b31a969e5ff94f36dba4aa33
Victor <CesarRK77@hotmail.com>
2025-11-28 01:24:43 -0600

    Cambio fri

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aqui realizo un cambio nuevo
+       Vamos a cambiar esto
         </p>
         <a
           className="App-link"
