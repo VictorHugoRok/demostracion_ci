ef7284add0881abea374bed403c022340f29b302
VictorHugoRok <a22216431@alumnos.uady.mx>
2025-12-05 17:42:48 -0600

    nuevo cambio

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-       Te quiero mucho labubu
+        <p> 
+          Cambio a mi programa
         </p>
         <a
           className="App-link"
